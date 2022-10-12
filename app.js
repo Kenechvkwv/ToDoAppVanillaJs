a4fa8b74afc819c355e3e1303bfcdf9078292f51
kenechvkwv <ubesiekene@gmail.com>
2022-10-12 16:18:34 +0100

    filter html

--- app.js
+++ app.js
@@ -43,7 +43,12 @@ const deleteCheck = (e) => {
   //Delete toDo
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.remove();
+    //Animation
+    todo.classList.add("fall");
+    //Delete after animation
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
   }
   //Add to ToDo List
   if (item.classList[0] === "complete-btn") {
