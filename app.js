e430d9f9615f4fb56da8963afc5f9310909f7e98
kenechvkwv <ubesiekene@gmail.com>
2022-10-12 13:37:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Functions
+const addTodo = (event) => {
+  //prevent form from submitting
+  event.preventDefault();
+
+  //ToDo Div, create todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //Create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //checkmark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //Append to List
+  todoList.appendChild(todoDiv);
+
+  //clear todo Input value
+  todoInput.value = " ";
+};
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
